f998631d8e94d01cd6a3d41cd635d9e72a7d6614
Carla Escobar <carlaprimola@outlook.es>
2024-01-18 09:25:52 +0100

    adding footer

--- react-chicha-rica-project/src/App.jsx
+++ react-chicha-rica-project/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import AnimatedCard from './components/AnimatedCard'
 
 function App() {
@@ -12,6 +11,7 @@ function App() {
     <>
       <Header/>
       <AnimatedCard/>
+      <Footer/>
     </>
   )
 }
---
+++ react-chicha-rica-project/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+           <img src="./img/iconos/logoPlanoBlanco.svg" alt="Logotipo Miguel Meixoeiro" />
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
