2a9f132dfbbaf9d0e26f752becdb31494b31e2d1
Carla Escobar <carlaprimola@outlook.es>
2024-01-21 12:27:52 +0100

    creating about page

--- react-chicha-rica-project/src/pages/About.jsx
+++ react-chicha-rica-project/src/pages/About.jsx
@@ -3,9 +3,20 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      {/* Contenido de la página Acerca de */}
-    </div>
+    <main className="about__container">
+      <figure className="about__figure">
+          <img className="about__img" src="../img/parthiv-sailesh-5VUI8uR8yy8-unsplash.jpg" alt="imagen-autor" />
+      </figure>
+      <article className="about__article">
+          <h2 className="about__title">MIGUEL MEIXOEIRO</h2>
+          <div className="about__description">
+              Se realizan sesiones fotográficas en estudio y exterior. Se editan y retocan fotografías. Capturo imágenes que comuniquen o transmitan una idea o personalidad. Esto aplica tanto para la fotografía de personas, como de productos, paisajes o mascotas. Se decía que las fotos robaban el alma y es por su capacidad de mostrar la esencia de lo fotografiado. CONTACTA CONMIGO por los distintos medios.
+          </div>
+            
+      </article>             
+        
+            
+    </main>
   );
 };
 
