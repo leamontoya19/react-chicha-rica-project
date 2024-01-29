6356f2ef5b0e56de4780bcf9cee284b530373330
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2024-01-29 13:53:24 +0100

    adding xicon component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import '../styles/Contact.css'; // Importa los estilos
-import { LogoHeaderW ,FbIcon, IgIcon, LnkdIcon, } from '../assets/icons';
+import React, { useState } from "react";
+import "../styles/Contact.css"; // Importa los estilos
+import { LogoHeaderW, FbIcon, IgIcon, LnkdIcon, XIcon } from "../assets/icons";
 const Contact = () => {
   const [formData, setFormData] = useState({
-    nombre: '',
-    email: '',
-    mensaje: ''
+    nombre: "",
+    email: "",
+    mensaje: "",
   });
 
-  const [formAlert, setFormAlert] = useState('');
+  const [formAlert, setFormAlert] = useState("");
 
   const cerrarVentana = () => {
     // Implementa la lógica para cerrar la ventana aquí
-    console.log('Ventana cerrada');
+    console.log("Ventana cerrada");
   };
 
   const validarFormulario = () => {
@@ -21,38 +21,42 @@ const Contact = () => {
     // Validación de nombre y apellido
     const nombreRegExp = /^[A-Z][a-z]+ [A-Z][a-z]+$/;
     if (!nombreRegExp.test(nombre)) {
-      setFormAlert('Por favor, introduce un nombre y apellido válidos (iniciando con mayúsculas).');
+      setFormAlert(
+        "Por favor, introduce un nombre y apellido válidos (iniciando con mayúsculas)."
+      );
       return;
     }
 
     // Validación de email
     const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegExp.test(email)) {
-      setFormAlert('Por favor, introduce una dirección de correo electrónico válida.');
+      setFormAlert(
+        "Por favor, introduce una dirección de correo electrónico válida."
+      );
       return;
     }
 
     // Validación de mensaje
     if (!mensaje.trim()) {
-      setFormAlert('Por favor, introduce un mensaje.');
+      setFormAlert("Por favor, introduce un mensaje.");
       return;
     }
 
     // Si todas las validaciones son exitosas
-    setFormAlert('Formulario enviado correctamente');
+    setFormAlert("Formulario enviado correctamente");
     // Aquí puedes enviar los datos del formulario a tu servidor o realizar otras acciones necesarias
   };
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.id]: e.target.value
+      [e.target.id]: e.target.value,
     });
   };
 
   return (
     <section className="sliderLateral" id="sliderLateralContainer">
-      <LogoHeaderW/>
+      <LogoHeaderW />
       <section className="slider__formulario">
         <form className="form" id="myForm">
           <a className="navicon" onClick={cerrarVentana} id="sliderRojo">
@@ -80,10 +84,14 @@ const Contact = () => {
             placeholder="Mensaje"
             value={formData.mensaje}
             onChange={handleChange}
-            style={{ resize: 'none' }}
+            style={{ resize: "none" }}
           ></textarea>
           <div className="center">
-            <button type="button" className="button-contact" onClick={validarFormulario}>
+            <button
+              type="button"
+              className="button-contact"
+              onClick={validarFormulario}
+            >
               Submit
             </button>
           </div>
@@ -96,7 +104,8 @@ const Contact = () => {
       <section className="redes">
         <FbIcon />
         <IgIcon />
-        <LnkdIcon/>
+        <LnkdIcon />
+        <XIcon />
         <img src="./img/twiter.svg" alt="" className="redes--formato twit" />
       </section>
     </section>
